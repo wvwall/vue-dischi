3d22bb9769ff76bcfcfd0bced2911e3759974bd0
wvwall <waltervelardo@gmail.com>
2021-04-10 13:45:57 +0200

    Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,19 @@ var app = new Vue({
     el: '#root',
     data: {
       albums : [],
+      albumsGenre : [],
+      select : "All"
+    },
+    methods: {
+        selectGenre: function () {
+            for (let i = 0; i < this.albums.length; i++) {
+              if (!this.albumsGenre.includes(this.albums[i].genre)) {
+                this.albumsGenre.push(this.albums[i].genre)
+              } else {
+                /* console.log("E' gia incluso"); */
+              }
+            }
+          }
     },
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
