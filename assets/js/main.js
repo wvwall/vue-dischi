f31ecb9b694705c7db05e142f68b8e266ed275bf
wvwall <waltervelardo@gmail.com>
2021-04-12 09:40:11 +0200

    Commenti

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ var app = new Vue({
     },
     methods: {
         selectGenre: function () {
+          /* scorro albums e pusho generi in array generi */
             for (let i = 0; i < this.albums.length; i++) {
               if (!this.albumsGenre.includes(this.albums[i].genre)) {
                 this.albumsGenre.push(this.albums[i].genre)
@@ -19,6 +20,7 @@ var app = new Vue({
           }
     },
     mounted() {
+      /* chiamata api con tutti i dati degli album */
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((response) => {
             this.albums = response.data.response;
